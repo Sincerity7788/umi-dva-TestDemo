9817961c9b7dbda089862a9e5ec45f9d01126093
lc <dei@live.cn>
2021-01-18 19:10:17 +0800

    修改处理px等于vw，排除antd

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'umi';
 import router from './src/router/index';
+// @ts-ignore
+import postcssPx2vw from 'postcss-px-to-viewport';
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
@@ -8,4 +10,14 @@ export default defineConfig({
     type: 'hash',
   },
   routes: router,
+  extraPostCSSPlugins: [
+    postcssPx2vw({
+      viewportWidth: 750,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      minPixelValue: 1,
+      exclude: /(\/|\\)(node_modules)(\/|\\)/,
+      // include: /\/src\//
+    }),
+  ],
 });
